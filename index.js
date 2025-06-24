49eef515aecd3c98573f5aa2a9fc1b8ea69315b6
eberriphath <riphatheber@gmail.com>
2025-06-25 00:47:19 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Karen';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not Karen'; // Will throw an error
+}
